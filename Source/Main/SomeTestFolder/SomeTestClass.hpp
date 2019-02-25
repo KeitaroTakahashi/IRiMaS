2d2b258118c1f1d5c42262df1f68f4d795cd29fd
Frédéric Dufeu <fredericdufeu@frederics-macbook-pro.local>
2019-02-25 13:56:35 +0000

    Test class added

---
+++ Source/Main/SomeTestFolder/SomeTestClass.hpp
@@ -0,0 +1,13 @@
+//
+//  SomeTestClass.hpp
+//  NodeComponentObject_Study - App
+//
+//  Created by Frédéric Dufeu on 25/02/2019.
+//
+
+#ifndef SomeTestClass_hpp
+#define SomeTestClass_hpp
+
+#include <stdio.h>
+
+#endif /* SomeTestClass_hpp */
