554607de192232bd8b572403b8a822e7df8f64fd
Frédéric Dufeu <fredericdufeu@frederics-macbook-pro.local>
2019-02-27 16:28:53 +0000

    Further file and folder creation for System / NodeObject

---
+++ Source/System/NodeObject/ObjectCreater/IRObjectCreaterBase/IRObjectCreaterBase.hpp
@@ -0,0 +1,13 @@
+//
+//  IRObjectCreaterBase.hpp
+//  NodeComponentObject_Study - App
+//
+//  Created by Frédéric Dufeu on 27/02/2019.
+//
+
+#ifndef IRObjectCreaterBase_hpp
+#define IRObjectCreaterBase_hpp
+
+#include <stdio.h>
+
+#endif /* IRObjectCreaterBase_hpp */
---
+++ Source/System/NodeObject/ObjectCreater/IRObjectFactory/IRObjectFactory.cpp
@@ -0,0 +1,8 @@
+//
+//  IRObjectFactory.cpp
+//  NodeComponentObject_Study - App
+//
+//  Created by Frédéric Dufeu on 27/02/2019.
+//
+
+#include "IRObjectFactory.hpp"
