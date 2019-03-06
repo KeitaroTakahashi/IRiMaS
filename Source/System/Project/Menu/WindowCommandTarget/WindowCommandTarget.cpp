3f80856a688e78d8f3e4513ef57945f7762e3db2
Frédéric Dufeu <fredericdufeu@frederics-macbook-pro.local>
2019-03-06 13:22:48 +0000

    Refactored WindowCommandTarget across .hpp and .cpp

--- Source/System/Project/Menu/WindowCommandTarget/WindowCommandTarget.cpp
+++ Source/System/Project/Menu/WindowCommandTarget/WindowCommandTarget.cpp
@@ -1,8 +1,70 @@
-//
-//  WindowCommandTarget.cpp
-//  NodeComponentObject_Study - App
-//
-//  Created by Frédéric Dufeu on 06/03/2019.
-//
 
 #include "WindowCommandTarget.hpp"
+
+
+
+
+WindowCommandTarget::WindowCommandTarget(ApplicationCommandManager& m) : commandManager(m)
+{
+    commandManager.registerAllCommandsForTarget(this);
+}
+
+
+void WindowCommandTarget::resized()
+{
+    
+}
+
+
+void WindowCommandTarget::paint(Graphics& g)
+{
+    
+}
+
+
+ApplicationCommandTarget* WindowCommandTarget::getNextCommandTarget()
+{
+    return findFirstTargetParentComponent();
+}
+
+
+void WindowCommandTarget::getAllCommands(Array<CommandID>& c)
+{
+    Array<CommandID> commands { CommandIDs::menuPreferenceWindow};
+    c.addArray (commands);
+}
+
+
+void WindowCommandTarget::getCommandInfo(CommandID commandID, ApplicationCommandInfo& result)
+{
+    switch (commandID)
+    {
+        case CommandIDs::menuPreferenceWindow:
+            result.setInfo ("Preference Window", "Sets the outer colour to red", "Window", 0);
+            result.addDefaultKeypress ('p', ModifierKeys::shiftModifier);
+            break;
+            
+        default:
+            break;
+    }
+}
+
+
+bool WindowCommandTarget::perform(const InvocationInfo& info)
+{
+    switch(info.commandID)
+    {
+        case CommandIDs::menuPreferenceWindow:
+            break;
+            
+        default:
+            return false;
+    }
+    
+    repaint();
+    return true;
+}
+
+
+
+
--- Source/System/Project/Menu/WindowCommandTarget/WindowCommandTarget.hpp
+++ Source/System/Project/Menu/WindowCommandTarget/WindowCommandTarget.hpp
@@ -8,70 +8,36 @@
 
 
 
-class WindowCommandTarget : public Component,
-public ApplicationCommandTarget
+
+class WindowCommandTarget : public Component, public ApplicationCommandTarget
 {
+    
 public:
-    WindowCommandTarget(ApplicationCommandManager& m) : commandManager(m)
-    {
-        commandManager.registerAllCommandsForTarget(this);
-    }
     
-    void resized() override
-    {
-        
-    }
+    WindowCommandTarget(ApplicationCommandManager& m);
     
-    void paint(Graphics& g)override
-    {
-        
-    }
+    void resized() override;
+    void paint(Graphics& g)override;
     
-    //==============================================================================
-    ApplicationCommandTarget* getNextCommandTarget() override
-    {
-        return findFirstTargetParentComponent();
-    }
+    ApplicationCommandTarget* getNextCommandTarget() override;
     
-    void getAllCommands(Array<CommandID>& c) override
-    {
-        Array<CommandID> commands { CommandIDs::menuPreferenceWindow};
-        c.addArray (commands);
-    }
+    void getAllCommands(Array<CommandID>& c) override;
     
-    void getCommandInfo (CommandID commandID, ApplicationCommandInfo& result) override
-    {
-        
-        switch (commandID)
-        {
-            case CommandIDs::menuPreferenceWindow:
-                result.setInfo ("Preference Window", "Sets the outer colour to red", "Window", 0);
-                result.addDefaultKeypress ('p', ModifierKeys::shiftModifier);
-                break;
-                
-            default:
-                break;
-        }
-    }
+    void getCommandInfo (CommandID commandID, ApplicationCommandInfo& result) override;
+    bool perform (const InvocationInfo& info) override;
     
-    bool perform (const InvocationInfo& info) override
-    {
-        switch(info.commandID)
-        {
-            case CommandIDs::menuPreferenceWindow:
-                break;
-                
-            default:
-                return false;
-        }
-        repaint();
-        return true;
-    }
     
 private:
+    
     ApplicationCommandManager& commandManager;
     
 };
 
 
+
+
 #endif /* WindowCommandTarget_hpp */
+
+
+
+
