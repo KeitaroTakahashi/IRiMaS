a54c8297356db8041541f888e841b0af589f35ed
Frédéric Dufeu <fredericdufeu@frederics-macbook-pro.local>
2019-03-25 13:02:22 +0000

    Restored a dummy ObjectFactory.h

---
+++ Source/ExternalObjects/ObjectFactory.h
@@ -0,0 +1,12 @@
+//
+//  ObjectFactory.h
+//  NodeComponentObject_Study
+//
+//  Created by Frédéric Dufeu on 25/03/2019.
+//
+
+#ifndef ObjectFactory_h
+#define ObjectFactory_h
+
+
+#endif /* ObjectFactory_h */
